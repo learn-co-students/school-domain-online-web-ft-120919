98109410c76c1c8f2bc0b3c6ae7791a0a9fc8070
nwscala <nwscala@gmail.com>
2019-12-17 16:50:50 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+    def initialize(school_name)
+        @school = school_name
+        @roster = {}
+    end
+    
+    def roster
+       @roster
+    end
+
+    def add_student(student_name, student_grade)
+        if @roster.key?(student_grade)
+            @roster[student_grade] << student_name
+        else
+            @roster[student_grade] = []
+            @roster[student_grade] << student_name
+        end
+    end
+
+    def grade(student_grade)
+        @roster[student_grade]
+    end
+
+    def sort
+        @roster.each do |student_grade, student_name_array|
+            @roster[student_grade] = student_name_array.sort
+        end
+    end
+end
