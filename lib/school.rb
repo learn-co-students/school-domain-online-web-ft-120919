dd50611b645c2578ed33632f83b67e28e4e9bac0
Alex Engelhart <asengelhart@yahoo.com>
2019-12-18 17:37:23 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  attr_accessor :school_name, :roster
+  def initialize(school_name)
+    @school_name = school_name 
+    @roster = {}
+  end 
+  def add_student(student_name, grade_num)
+    @roster[grade_num] = [] if !@roster.include?(grade_num)
+    @roster[grade_num] << student_name if !@roster[grade_num].include?(student_name)
+  end 
+  def grade(grade_num)
+    @roster[grade_num]
+  end 
+  def sort
+    @roster.inject({}) do |memo, (key, val_array)| 
+      memo[key] = val_array.sort
+      memo
+    end 
+  end 
+end 
