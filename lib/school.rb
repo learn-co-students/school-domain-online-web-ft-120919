43ab1a6a7c599edd5aa7a4f5e7e047e984289afd
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-18 22:51:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
-# code here!
+class School
+  attr_accessor :name 
+  def initialize(name)
+    @name = name
+  end 
+  school.roster { }
+  
+  def add_student(student)
+    @student = student 
+  end 
+student << roster
+end 
